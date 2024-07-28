5d18e5b96eb838932947b38cc952556b833cfb90
Jakub Hajduk <devhajduk@gmail.com>
2024-07-28 18:17:52 +0200

    fix: fix context abd subcontext creation

--- src/core/context.ts
+++ src/core/context.ts
@@ -2,35 +2,93 @@ import { Logger, type LoggerOptions } from './logger'
 import { OutputCollection, type OutputCollectionOptions } from './output-files-collection'
 
 export interface ContextOptions {
-  files?: Partial<OutputCollectionOptions>
-  logger?: Partial<LoggerOptions>
+  /**
+   * Whether files should be written to disk or not.
+   */
+  dryRun: boolean
+  /**
+   * Whether log messages should be printed
+   */
+  silent: boolean
+  /**
+   * Out dir for files to write
+   * equivalent of setting logLevel to 0.
+   * @default ./
+   */
+  outDir: string
+  /**
+   * The level of messages to be shown:
+   * 0: Fatal and Error
+   * 1: Warnings
+   * 2: Normal logs
+   * 3: Informational logs, success, fail, ready, start, ... (default)
+   * 4: Debug logs
+   * 5: Trace logs
+   *
+   * @default 3
+   */
+  logLevel: 0 | 1 | 2 | 3 | 4 | 5;
+  /**
+   * Whether changes to files shall be logged for debuging purposes
+   */
+  logChanges: boolean
+  /**
+   * Whether debug mode should be active.
+   * This option sets logLevel to 5 and logChanges to true.
+   */
+  debug: boolean
 }
 
 export interface Context {
   cwd: string;
+  name: string;
   options: Partial<ContextOptions>
   files: OutputCollection
   logger: Logger,
   createSubContext: (name: string) => Context
+  parentContext: Context | null,
+  childContexts: Context[]
 }
 
 export function createContext (options?: Partial<ContextOptions>): Context {
-  const logger = Logger.getInstance(options.logger)
-  const files = OutputCollection.getInstance(options.files)
+  const loggerOptions: LoggerOptions = {
+    level: options?.silent ? 0 : (options?.logLevel || 3)
+  }
 
-  const context = {
+  const outputCollectionOptions: OutputCollectionOptions = {
+    dry: options?.dryRun || false,
+    outDir: options?.outDir || './',
+    logChanges: options.debug || options?.logChanges || false
+  }
+
+  const logger = new Logger(loggerOptions)
+  const files = new OutputCollection(outputCollectionOptions)
+
+  const context: Context = {
+    name: 'root',
     cwd: process.cwd(),
     options: options || {},
-    parent: null,
+    parentContext: null,
+    childContexts: [],
     logger,
     files,
     createSubContext(name: string): Context {
-      return {
+      const newContext = {
         ...this,
-        logger: this.logger.createSubLogger(name)
+        name,
+        parentContext: this,
+        logger: this.logger.createSubLogger(name),
+        files: this.files.withOrigin(name)
       }
+
+      this.childContexts.push(newContext)
+
+      return newContext
     }
   }
 
   return context
 }
+
+
+
--- src/core/output-files-collection.ts
+++ src/core/output-files-collection.ts
@@ -8,6 +8,7 @@ import { ChangeLog } from './change-log'
 export interface OutputCollectionOptions {
   outDir: string;
   dry: boolean;
+  logChanges: boolean
 }
 
 export interface OutputCollectionEventMap {
@@ -24,19 +25,23 @@ export class OutputCollection {
   private emitter = new TypedEmitter<OutputCollectionEventMap>();
   private changeLog = new ChangeLog()
   public files: Map<string, string> = new Map();
+  public origin?: string;
+
+  public rootCollection = this
 
   private options: OutputCollectionOptions = {
     outDir: dirname(fileURLToPath(import.meta.url)),
-    dry: false
+    dry: false,
+    logChanges: false
   };
 
-  on = this.emitter.on
-  once = this.emitter.once
+  on = this.rootCollection.emitter.on
+  once = this.rootCollection.emitter.once
 
   constructor(options?: Partial<OutputCollectionOptions>) {
     this.options = { ...this.options, ...options};
-    this.on = this.emitter.on.bind(this);
-    this.once = this.emitter.once.bind(this);
+    this.rootCollection.on = this.rootCollection.emitter.on.bind(this);
+    this.rootCollection.once = this.rootCollection.emitter.once.bind(this);
   }
 
   static getInstance(options?: Partial<OutputCollectionOptions>) {
@@ -47,11 +52,19 @@ export class OutputCollection {
     return OutputCollection.instance
   }
 
+  public withOrigin(name: string) {
+    const oc = new OutputCollection(this.options)
+    oc.origin = name
+    oc.rootCollection = this.rootCollection
+
+    return oc
+  }
+
   public get(path: string): [string, string]
   public get(predicate: (path: string, contents: string) => boolean): [string, string]
   public get(pathOrPredicate: string | ((path: string, contents: string) => boolean)): [string, string] {
     if(typeof pathOrPredicate === 'function') {
-      for(let file of this.files) {
+      for(let file of this.rootCollection.files) {
         if (pathOrPredicate(...file)) {
           return file
         }
@@ -59,40 +72,43 @@ export class OutputCollection {
       return
     }
 
-    if (this.files.has(pathOrPredicate)) {
-      return [pathOrPredicate, this.files.get(pathOrPredicate)]
+    if (this.rootCollection.files.has(pathOrPredicate)) {
+      return [pathOrPredicate, this.rootCollection.files.get(pathOrPredicate)]
     }
   }
 
-  public write(path: string, contents: string, origin?: string) {
-    this.files.set(path, contents);
-    this.changeLog.add(path, contents, origin)
-    this.emitter.emit('file:stacked',  { path, origin, contents });
+  public write(path: string, contents: string, origin: string = this.origin) {
+    this.rootCollection.files.set(path, contents);
+    if (this.options.logChanges) {
+      this.rootCollection.changeLog.add(path, contents, origin)
+    }
+    this.rootCollection.emitter.emit('file:stacked',  { path, origin, contents });
   }
 
   public getChanges(path: string) {
-    return this.changeLog.get(path)
+    if (!this.options.logChanges) return
+    return this.rootCollection.changeLog.get(path)
   }
 
   public writeFilesToDisk(options?: Partial<OutputCollectionOptions>) {
     const finalOptions: OutputCollectionOptions = {
-      ...this.options,
+      ...this.rootCollection.options,
       ...options
     }
     let index = 1;
-    this.emitter.emit('write:start', { index, total: this.files.size });
-    this.files.forEach(async (contents, file) => {
+    this.rootCollection.emitter.emit('write:start', { index, total: this.rootCollection.files.size });
+    this.rootCollection.files.forEach(async (contents, file) => {
       const path = resolve(finalOptions.outDir, file)
       try {
-        if (!this.options.dry) {
+        if (!this.rootCollection.options.dry) {
           outputFileSync(path, contents);
         }
-        this.emitter.emit('write:file-written', { path, index, total: this.files.size });
+        this.rootCollection.emitter.emit('write:file-written', { path, index, total: this.rootCollection.files.size });
       } catch (error) {
-        this.emitter.emit('write:file-error', { path, index, total: this.files.size, error });
+        this.rootCollection.emitter.emit('write:file-error', { path, index, total: this.rootCollection.files.size, error });
       }
       index++;
     });
-    this.emitter.emit('write:done', { index, total: this.files.size });
+    this.rootCollection.emitter.emit('write:done', { index, total: this.rootCollection.files.size });
   }
 }
